e7dfc4583300ecdaa4dec34295a28849ee700b86
Dasha96 <dashic96@mail.ru>
2014-04-10 20:38:06 +0400

    step4

---
+++ head.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+/** 
+     * Перечисление.
+     * Более подробное описание перечисление.
+     */
+void P_Sort ( char *Mass, int n );
+void MergeSort(char *A, int first, int last);
+void Merge(char *A, int first, int last);
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "head.h"
+
+void main()
+/** 
+     * Перечисление.
+     * Более подробное описание перечисление.
+     */
+{
+	char *Mass;
+	int n;
+	int i;
+	srand(time(NULL));
+	printf( "Input N: " );
+	scanf ( "%d", &n );
+	Mass = new char ( n );
+	for( i = 1; i < n; i++ )
+	{
+		Mass[ i ] = rand()%255;
+	}
+	for( i = 1; i < n; i++ )
+	{
+		printf( "%d\n", Mass[ i ] );
+	}
+	P_Sort( Mass, n );
+	MergeSort( Mass, 1, n);
+	for( i = 1; i < n; i++ )
+	{
+		printf( "%d\n", Mass[ i ] );
+	}
+	system( "pause" );
+}
